0f569c74bdaa3927a3597dc67d9c807c6f6df38b
KarDB <48925071+KarDB@users.noreply.github.com>
2023-12-07 16:59:17 +0100

    Transtioning to class for ExecutionBlock

--- exequtor/generate_instructions.py
+++ exequtor/generate_instructions.py
@@ -1,13 +1,23 @@
 import yaml
 import numpy as np
 from pathlib import Path
-from typing import Dict, Any
+from typing import Dict, Any, List
 filename = 'optmeasurement.yml'
 
 
-def writeyamlfile(params: Dict[str, Any], savename: str) -> None:
-    with open(savename, 'w', encoding='utf-8') as file:
-        yaml.dump(params, file)
+class ExecutionBlock:
+    def __init__(self, base_file: str, sweep_params: List[str], sweep_vals: List[str]):
+        self.base_file = base_file
+        self.sweep_params = sweep_params
+        self.sweep_vals = sweep_vals
+        self.params: Dict[str, Any]
+
+    def write_yaml_file(self, savename: str) -> None:
+        with open(savename, 'w', encoding='utf-8') as file:
+            yaml.dump(self.params, file)
+
+    def make_range_sweep_value(self, sweep_parameter_index):
+        pass
 
 
 with open(filename, 'r', encoding='utf-8') as file:
@@ -24,5 +34,9 @@ with open(filename, 'r', encoding='utf-8') as file:
                 sweepvalue = eval('params'+filedata['M_1']['sweep_params'][0])
                 print(i, k, sweepvalue)
                 savename = 'File_' + str(k)
-                writeyamlfile(params, savename)
+                # write_yaml_file(params, savename)
     # make measurement nAot hardcoded for i in number Ms?
+
+execution_block = ExecutionBlock(filedata['M_1']['base_file'],
+                                 filedata['M_1']['sweep_params'],
+                                 filedata['M_1']['sweep_vals']),
