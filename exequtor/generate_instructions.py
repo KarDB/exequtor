90620ccbaf44c30d6e89ccc8c68e17f7c72627d4
Julia Draeger <116548887+Judr9@users.noreply.github.com>
2023-12-13 15:19:54 +0100

    Functioning file generation with higher modularity towards differnt sweep lengths and constant parameters

--- exequtor/generate_instructions.py
+++ exequtor/generate_instructions.py
@@ -1,18 +1,17 @@
-#import yaml
 import numpy as np
 import ruamel.yaml
-#from ruamel.yaml import YAML, YAMLError
 import numpy as np
 from pathlib import Path
 from typing import Dict, Any, List
 from time import sleep
-from math import log10, floor
+from math import log10, floor, lcm
+
 
 yaml = ruamel.yaml.YAML(typ = 'safe')
 yaml.preserve_quotes = True
 
-#filename = 'optmeasurement.yml'
-#saveplace_busy_document = 'status.txt'   #'/home/karl/.qupyt/status.txt'
+filename = 'optmeasurement.yml'
+saveplace_busy_document = 'status.txt'   #'/home/karl/.qupyt/status.txt'
 
 def wait_while_busy():
     wait = True
@@ -24,8 +23,8 @@ def wait_while_busy():
             sleep(1)
 
 def round_to_n(value:float, n:int) -> float:
-     x_rounded = round(value, -int(floor(log10(value))) + (n - 1))
-     return x_rounded
+     value_rounded = round(value, -int(floor(log10(value))) + (n - 1))
+     return value_rounded
 
 
 class ExecutionBlock:
@@ -34,9 +33,13 @@ class ExecutionBlock:
         self.sweep_params = sweep_params
         self.sweep_vals = sweep_vals
         self.params: Dict[str, Any]
+    #    self.sweep_function: List[str]
 
     def assign_params(self,params):
         self.params = params
+
+    #def assign_function_single(self,params):
+    #    self.sweep_function = params
 
     def generate_precise_name(self, value:str) -> str:
         param_name = ''
@@ -46,44 +49,74 @@ class ExecutionBlock:
         return savename
 
     def write_yaml_file(self, savename: str) -> None:
-        with open(savename, 'w', encoding='utf-8') as file:
+        with open('dumpjamls/'+savename, 'w', encoding='utf-8') as file:
             yaml.dump(self.params, file)
 
     def make_range_sweep_value_single(self, sweep_parameter_index:int) -> List[float]:
         rangevals = self.sweep_vals
-        rangevals = rangevals[int(sweep_parameter_index)].split(',')
-        vals = np.linspace(float(rangevals[0]), float(rangevals[1]), int(rangevals[2]))
-        return vals
+        rangevals = rangevals[int(sweep_parameter_index)].split(';')
+        #vals = np.linspace(float(rangevals[0]), float(rangevals[1]), int(rangevals[2]))
+        return rangevals
 
     def make_range_sweep_value_all(self) -> np.ndarray:
         number_parallelsweepvals = len(self.sweep_params)
-        number_sweep_values = len(self.make_range_sweep_value_single(0))
-        individual_sweep_vals = np.zeros((number_parallelsweepvals, number_sweep_values))
+        #number_sweep_values = len(self.make_range_sweep_value_single(0))
+
+        sweep = np.zeros(number_parallelsweepvals, dtype=bool)
+        max_sweep_number = [0] * number_parallelsweepvals
         for i in range(number_parallelsweepvals):
-            vals = self.make_range_sweep_value_single(i)
-            for k, val in enumerate(vals):
-                individual_sweep_vals[i,k] = val
+            if ";" in self.sweep_vals[i]:
+                sweep[i] = True
+                rangevals = self.make_range_sweep_value_single(i)
+                max_sweep_number[i] = int(rangevals[2])
+            else:
+                max_sweep_number[i] = int(1)
+        number_sweep_values = lcm(*max_sweep_number)
+        individual_sweep_vals = np.zeros((number_parallelsweepvals, number_sweep_values))
+
+        for i in range(number_parallelsweepvals):
+            if sweep[i] == True:
+                rangevals = self.make_range_sweep_value_single(i)
+                vals = np.linspace(float(rangevals[0]), float(rangevals[1]), number_sweep_values)
+                for k, val in enumerate(vals):
+                    individual_sweep_vals[i,k] = val
+            elif sweep[i] == False:
+                vals = [float(self.sweep_vals[i])]*number_sweep_values
+                for k, val in enumerate(vals):
+                    individual_sweep_vals[i,k] = val
+        print(individual_sweep_vals)
         return individual_sweep_vals
 
 
-#if __name__ == '__main__:'
+
 def make_yaml_files_for_sweeps(filename:str, saveplace_busy_document:str):
     with open(filename, 'r', encoding='utf-8') as file:
         filedata = yaml.load(file)
         for i, key in enumerate(filedata.keys()):
-            execution_block = ExecutionBlock(filedata[key]['base_file'],
-                                    filedata[key]['sweep_params'],
-                                    filedata[key]['sweep_vals'])
-            rangevals = execution_block.make_range_sweep_value_all()
-            with open(execution_block.base_file, 'r', encoding='utf-8') as p:
-                params = yaml.load(p)
-                execution_block.assign_params(params)
-                for j in range(np.shape(rangevals)[1]):
-                    wait_while_busy()
-                    value_name_for_save = ''
-                    for k in range(len(execution_block.sweep_params)):
-                        exec('execution_block.params'+execution_block.sweep_params[k]+f' = {rangevals[k,j]}')
-                        value_name_for_save += '_' + str(round_to_n(rangevals[k,j], 8))
-                    savename = execution_block.generate_precise_name(value_name_for_save)
-                    execution_block.params['experiment_type'] = savename
-                    execution_block.write_yaml_file(savename+'.yaml')
+            if 'sweep_params' in filedata[key]:
+
+                execution_block = ExecutionBlock(filedata[key]['base_file'],
+                                                filedata[key]['sweep_params'],
+                                                filedata[key]['sweep_vals'])
+                rangevals = execution_block.make_range_sweep_value_all()
+                with open(execution_block.base_file, 'r', encoding='utf-8') as p:
+                    params = yaml.load(p)
+                    execution_block.assign_params(params)
+                    for j in range(np.shape(rangevals)[1]):
+                        wait_while_busy()
+                        value_name_for_save = ''
+                        for k in range(len(execution_block.sweep_params)):
+                            exec('execution_block.params'+execution_block.sweep_params[k]+f' = {rangevals[k,j]}')
+                            value_name_for_save += '_' + str(round_to_n(rangevals[k,j], 8))
+                        savename = execution_block.generate_precise_name(value_name_for_save)
+                        execution_block.params['experiment_type'] = savename
+                        execution_block.write_yaml_file(savename+'.yaml')
+            else:
+                with open(filedata[key]['base_file'], 'r', encoding='utf-8') as p:
+                    params = yaml.load(p)
+                    savename = filedata[key]['base_file']
+                    with open('dumpjamls/'+savename, 'w', encoding='utf-8') as file:
+                            yaml.dump(params, file)
+
+if __name__ == '__main__':
+    make_yaml_files_for_sweeps(filename, saveplace_busy_document)
